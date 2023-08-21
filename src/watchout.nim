a2aaaf3efcf4efa70c3d428e127d3d7a837ca52d
George Lemon <georgelemon@protonmail.com>
2023-08-21 07:01:56 +0300

    update
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/watchout.nim
+++ src/watchout.nim
@@ -1,75 +1,76 @@
-import std/[tables, locks, typeinfo]
-
-from std/times import Time
-from std/os import getLastModificationTime, sleep, fileExists, splitPath, execShellCmd
-from std/strutils import `%`, strip, split
-from std/osproc import execProcess, poStdErrToStdOut, poUsePath
+import std/[tables, typeinfo, times, os, strutils]
 
 export typeinfo
 
 type
-    File* = ref object
-        path: string
-        lastModified: Time
+  File* = ref object
+    path: string
+    lastModified: Time
 
-    Callback* = proc(file: File) {.closure.} 
+  Callback* = proc(file: File) {.closure.} 
 
-    Watchout* = object
-        ref_files: seq[string]
-        files: OrderedTable[string, File]
-        cleanOutput: bool
-        callback: Callback
-    
-    FSNotifyException* = object of CatchableError
+  Watchout* = object
+    entries: OrderedTableRef[string, File]
+    callback: Callback
+  
+  WatchoutException* = object of CatchableError
 
 when not compileOption("threads"):
-    raise newException(FSNotifyException, "Watchout requires --threads:on")
+  raise newException(WatchoutException, "Watchout requires --threads:on")
 
 var
-    l: Lock
-    m {.threadvar}: Watchout
-    thr: Thread[(seq[string], Callback, int)]
+  m {.threadvar}: Watchout
+  thr: Thread[(seq[string], Callback, int)]
 
 proc getPath*[F: File](f: F): string = f.path
 proc getName(f: string): string = splitPath(f).tail
 proc getName*[F: File](f: F): string = getName(f.path)
 
-# proc cleanOutputIfEnabled(monitor: var Watchout) =
-#     ## Clean previous output in terminal screen
-#     if monitor.cleanOutput: discard execShellCmd("clear")
+template walkHandler() {.dirty.} =
+  monitor.entries[entry] = new File
+  monitor.entries[entry].path = entry
+  monitor.entries[entry].lastModified = getLastModificationTime(entry)
 
-proc addFile*[W: Watchout](monitor: var W, f: string) =
-    ## Add a new file for monitoring live changes with callback procedure
-    if f.fileExists():
-        if not monitor.files.hasKey(f):
-            monitor.files[f] = new File
-            monitor.files[f].path = f
-            monitor.files[f].lastModified = getLastModificationTime(f)
-            monitor.ref_files.add(f)
-        else: raise newException(FSNotifyException, "File has already been monitored once")
-    else: raise newException(FSNotifyException, "File does not exist:\n$1" % [f])
+proc add(monitor: var Watchout, f: string) {.thread.} =
+  if likely(f.fileExists):
+    if likely(not monitor.entries.hasKey(f)):
+      var entry = f
+      walkHandler()
+    else: raise newException(WatchoutException, "Duplicate file in Watchout monitor:\n$1" % [f])
+  elif f.dirExists:
+    for entry in walkDirRec(f):
+      if entry.isHidden or monitor.entries.hasKey(f): continue
+      walkHandler
+  elif f.parentDir.dirExists:
+    for entry in walkPattern(f):
+      if entry.isHidden or monitor.entries.hasKey(f): continue
+      walkHandler
+  else: raise newException(WatchoutException, "File does not exist:\n$1" % [f])
 
 proc start(arg: (seq[string], Callback, int)) {.thread.} =
-    {.gcsafe.}:
-        m.callback = arg[1]
-        for path in arg[0]:
-            m.addFile path
-        var i = 1
-        let allFiles = arg[0].len
-        while true:
-            for k, file in m.files.mpairs():
-                if likely(fileExists(k)):
-                    let updateLastModified = getLastModificationTime(file.path)
-                    if file.lastModified != updateLastModified:
-                        # m.cleanOutputIfEnabled()
-                        m.callback(file)
-                        file.lastModified = updateLastModified
-                        m.files[file.path] = file 
-                else:
-                    echo "File $1 has been deleted" % [k.getName]
-            sleep(arg[2])
+  {.gcsafe.}:
+    m.callback = arg[1]
+    m.entries = newOrderedTable[string, File]()
+    for p in arg[0]:
+      m.add(p)
+    while true:
+      for path, file in m.entries.mpairs():
+        if likely(path.fileExists or path.dirExists):
+          let updateLastModified = getLastModificationTime(file.path)
+          if file.lastModified != updateLastModified:
+            m.callback(file)
+            file.lastModified = updateLastModified
+            m.entries[file.path] = file 
+        else:
+          echo "File $1 has been deleted" % [path.getName]
+          m.entries.del(path)
+      sleep(arg[2])
 
 proc startThread*(callback: Callback, files: seq[string], ms: int, shouldJoinThread = false) =
-    createThread(thr, start, (files, callback, ms))
-    if shouldJoinThread:
-        joinThread(thr)
+  ## Run Watchout in a separate thread 
+  createThread(thr, start, (files, callback, ms))
+  if shouldJoinThread:
+    joinThread(thr)
+
+# proc spawnProcess*(callback: Callback, files: seq[string], ms: int) =
+#   spawn start((files, callback, ms))
--- watchout.nimble
+++ watchout.nimble
@@ -2,15 +2,10 @@
 
 version       = "0.1.0"
 author        = "George Lemon"
-description   = "⚡️ Just... yellin' for changes! File System Monitor for devs"
+description   = "⚡️ Just... yellin' for changes! A fast file system monitor, browser syncing & reloader"
 license       = "MIT"
 srcDir        = "src"
 
 # Dependencies
 
 requires "nim >= 1.6.0"
-
-
-task dev, "Compile for development":
-    echo "\n✨ Compiling for dev" & "\n"
-    exec "nimble build --gc:arc -d:useMalloc"
